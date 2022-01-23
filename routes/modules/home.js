2909fbcdad2c78db8dcb6451a407db228381ac59
Broderier <nnm.kb.90@gmail.com>
2022-01-24 02:33:38 +0900

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { append } = require('express/lib/response')
 const router = express.Router()
 
 const Restaurant = require('../../models/restaurant')
@@ -8,6 +9,33 @@ router.get('/', (req, res) => {
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
+})
+
+router.post('/', (req, res) => {
+  const sortMethod = req.body.sortingMethod
+  let sort = ''
+  switch (sortMethod) {
+    case '1':
+      sort = { name_en: 'asc' }
+      break
+    case '2':
+      sort = { name_en: 'desc' }
+      break
+    case '3':
+      sort = { category: 'desc' }
+      break
+    case '4':
+      sort = { location: 'desc' }
+      break
+    default:
+      sort = { _id: 'asc' }
+  }
+  Restaurant.find()
+    .lean()
+    .sort(sort)
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
+
 })
 
 router.get('/search', (req, res) => {
